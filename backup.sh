45d4643910ba54569d3d8074224a7aadab9d0afb
Nikolay Zhuravlev <nikolay.a.zhuravlev@yandex.ru>
2025-05-05 15:00:23 +0300

    Remove unused

--- backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-now=$(date +"%Y-%m-%d-%H-%M")
-docker run --rm --entrypoint "" \
-  -v /opt/backup:/backup \
-  --network="shvirtd-example-python_backend" \
-  my-mysqldump \
-  mysqldump --opt -hmysql -uroot -p${1} --result-file=/backup/${now}_virtd.sql virtd
